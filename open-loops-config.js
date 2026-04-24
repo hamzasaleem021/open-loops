f9d355d1f8b0233bf7a40ddd34eb11827b0886d9
hamzasaleem021 <hamzasaleem021@gmail.com>
2026-04-24 05:13:03 +0500

    Add files via upload

--- open-loops-config.js
+++ open-loops-config.js
@@ -2,8 +2,8 @@ window.OL_CONFIG = Object.freeze({
   APP_NAME: 'Open Loops',
   SUPABASE_URL: 'https://ujscnncechcwqhidzdgd.supabase.co',
   SUPABASE_KEY: 'sb_publishable_5HOKU2MF9V41NMhnYntyGw_8J0rwUZh',
-  // Leave null to use the current page URL automatically on localhost or GitHub Pages.
-  EMAIL_REDIRECT_TO: null,
+  // Use the deployed app URL explicitly so magic-link callbacks are stable.
+  EMAIL_REDIRECT_TO: 'https://hamzasaleem021.github.io/open-loops/open-loops.html',
 
   STORAGE_KEY: 'open_loops_data',
 
